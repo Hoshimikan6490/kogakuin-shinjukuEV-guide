820abbeed67bc390b7f5dac6afa029a72a11a00c
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-04-09 23:35:23 +0900

    feat: implement Express server with static file serving and route handling

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,27 @@
 module.exports = async () => {
-  console.log("Frontend is starting...");
+  const express = require("express");
+  const app = express();
+  const path = require("path");
+
+  app.use("/images", express.static(`${__dirname}/images`));
+
+  app.get("/style.css", async function (req, res) {
+    res.sendFile(`${__dirname}/page/style.css`);
+  });
+
+  app.get("/script/goTop.js", async function (req, res) {
+    res.sendFile(`${__dirname}/page/script/goTop.js`);
+  });
+
+  app.get("/script/select.js", async function (req, res) {
+    res.sendFile(`${__dirname}/page/script/select.js`);
+  });
+
+  app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/page/index.html`);
+  });
+
+  app.listen(3000, function () {
+    console.log(`[NodeJS] Application Listening on Port 3000`);
+  });
 };
