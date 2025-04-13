96ea08a0037085ce625a68f02879be89907a2c55
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-04-13 11:05:31 +0900

    feat: remove HTML plugin configuration from Vite setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,6 @@ import { defineConfig } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
-  plugins: [
-    createHtmlPlugin({
-      inject: {
-        data: {
-          charset: '<meta charset="utf-8">',
-        },
-      },
-    }),
-  ],
   server: {
     port: 3000,
   },
