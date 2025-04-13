dd9e50c7a46c6670a636e4a5f8d5dbcb69079306
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-04-13 11:05:22 +0900

    chore: initialize backend package.json with dependencies and scripts

--- backend/index.js
+++ backend/index.js
@@ -1,45 +1,46 @@
-module.exports = async () => {
-  const express = require("express");
-  const app = express();
-  const cors = require("cors");
-  const fs = require("fs").promises;
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const fs = require("fs").promises;
+require("dotenv").config();
 
-  // CORSの有効化
-  app.use(cors({ origin: "http://localhost:3000" }));
+const frontendApiUrl = process.env.FRONTEND_API_URL;
 
-  app.get("/roomData", async function (req, res) {
-    try {
-      let room = await fs.readFile(`${__dirname}/roomData.json`, "utf-8");
-      room = JSON.parse(room);
+// CORSの有効化
+app.use(cors({ origin: `http://${frontendApiUrl}` }));
 
-      let building = req.query.building;
-      let floor = req.query.floor;
-      if (!building && floor) {
-        // 階の指定のみがあった場合
-        room.A = {
-          [floor]: room.A[floor],
-        };
-        room.B = {
-          [floor]: room.B[floor],
-        };
-      } else if (building && !floor) {
-        // 建物の指定のみがあった場合
-        room = room[building];
-        if (!room) {
-          room = {};
-        }
-      } else if (building && floor) {
-        // 階と建物の両方の指定があった場合
-        room = room[building][floor];
+app.get("/roomData", async function (req, res) {
+  try {
+    let room = await fs.readFile(`${__dirname}/roomData.json`, "utf-8");
+    room = JSON.parse(room);
+
+    let building = req.query.building;
+    let floor = req.query.floor;
+    if (!building && floor) {
+      // 階の指定のみがあった場合
+      room.A = {
+        [floor]: room.A[floor],
+      };
+      room.B = {
+        [floor]: room.B[floor],
+      };
+    } else if (building && !floor) {
+      // 建物の指定のみがあった場合
+      room = room[building];
+      if (!room) {
+        room = {};
       }
-      return res.send(room);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Internal Server Error");
+    } else if (building && floor) {
+      // 階と建物の両方の指定があった場合
+      room = room[building][floor];
     }
-  });
+    return res.send(room);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
-  app.listen(3030, function () {
-    console.log(`[backend] Application Listening on Port 3030`);
-  });
-};
+app.listen(3030, function () {
+  console.log(`[backend] Application Listening on Port 3030`);
+});
