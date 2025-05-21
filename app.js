099999bf1d8724eb7d4e1fc1f269c09d02ba7661
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-05-21 11:59:09 +0900

    /searchに経路取得処理を追加

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const fs = require("fs").promises;
 const path = require("path");
 const helmet = require("helmet");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 // ポートの設定
 let port = 80;
@@ -27,30 +29,48 @@ app.get("/", (req, res) => {
 
 app.get("/roomData", async (req, res) => {
   try {
-    let room = await fs.readFile(`${__dirname}/db/roomData.json`, "utf-8");
-    room = JSON.parse(room);
+    let roomDB = await fs.readFile(`${__dirname}/db/roomData.json`, "utf-8");
+    roomDB = JSON.parse(roomDB);
 
+    // 部屋番号から他の情報を取得する
+    let roomNumber = req.query.room;
+    // 正しい部屋番号形式か確認
+    if (roomNumber) {
+      if (roomNumber.match(/^[AB]-\d{4}$/)) {
+        let roomInfo = {};
+        roomInfo.building = roomNumber.split("-")[0];
+        roomInfo.floor = String(
+          Number(roomNumber.split("-")[1].substring(0, 2))
+        );
+        roomInfo.room = roomNumber;
+        return res.send(roomInfo);
+      } else {
+        return res.send({});
+      }
+    }
+
+    // 建物やフロア番号から取得する
     let building = req.query.building;
     let floor = req.query.floor;
     if (!building && floor) {
       // 階の指定のみがあった場合
-      room.A = {
-        [floor]: room.A[floor],
+      roomDB.A = {
+        [floor]: roomDB.A[floor],
       };
-      room.B = {
-        [floor]: room.B[floor],
+      roomDB.B = {
+        [floor]: roomDB.B[floor],
       };
     } else if (building && !floor) {
       // 建物の指定のみがあった場合
-      room = room[building];
-      if (!room) {
-        room = {};
+      roomDB = roomDB[building];
+      if (!roomDB) {
+        roomDB = {};
       }
     } else if (building && floor) {
       // 階と建物の両方の指定があった場合
-      room = room[building][floor];
+      roomDB = roomDB[building][floor];
     }
-    return res.send(room);
+    return res.send(roomDB);
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
@@ -62,7 +82,17 @@ app.get("/search", async (req, res) => {
   let routeData = await fs.readFile(`${__dirname}/db/routeData.json`, "utf-8");
   routeData = JSON.parse(routeData);
 
-  res.status(503).send("Service Unavailable");
+  // 部屋番号から他データを取得
+  await fetch(`http://localhost:${port}/roomData?room=${room}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return res.send(routeData[data.building][data.floor][data.room]);
+    });
 });
 
 app.listen(port, function () {
