e2dbdb2176cc809d4e89fcbc18b0aeb6348f6dd4
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-06-11 11:40:03 +0900

    fix: cacheClear.shのコマンドを修正し、正しい構文に統一

--- cacheClear.sh
+++ cacheClear.sh
@@ -4,12 +4,12 @@
 docker-compose down --volumes --remove-orphans
 
 # ビルドキャッシュ削除
-docker-builder prune -af
+docker builder prune -af
 
 # 未使用ネットワーク削除
-docker-network prune -f
+docker network prune -f
 
 # 未使用ボリューム削除
-docker-volume prune -f
+docker volume prune -f
 
 echo "Docker Compose のキャッシュをすべて削除しました。"
