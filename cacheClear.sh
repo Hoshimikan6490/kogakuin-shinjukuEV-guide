94b64bc59051e68dfdc828e3b3f42667e079d3ed
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-06-11 11:42:14 +0900

    fix: cacheClear.shのコマンドにsudoを追加し、権限の問題を解決

--- cacheClear.sh
+++ cacheClear.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Docker Compose の全サービス停止・削除
-docker-compose down --volumes --remove-orphans
+sudo docker-compose down --volumes --remove-orphans
 
 # ビルドキャッシュ削除
-docker builder prune -af
+sudo docker builder prune -af
 
 # 未使用ネットワーク削除
-docker network prune -f
+sudo docker network prune -f
 
 # 未使用ボリューム削除
-docker volume prune -f
+sudo docker volume prune -f
 
 echo "Docker Compose のキャッシュをすべて削除しました。"
