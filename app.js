b268e2e04a7ab97efedea3dca992e5427e576008
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2025-07-09 11:41:11 +0900

    feat: 部屋番号の確認機能を改善し、APIエンドポイントを整理

--- app.js
+++ app.js
@@ -10,18 +10,21 @@ let port = 80;
 // View engineをejsに設定
 app.set("view engine", "ejs");
 
+// JSONの受け取りを許可
+app.use(express.json());
+
 // 必要なデータを準備
 app.use(express.static("views"));
 app.use("views", express.static("views"));
 
 // 部屋番号が正しいか確認するfunction
-function isAvailableRoomID(roomDB, room) {
+function isAvailableRoomID(roomDB, roomID) {
   // 形式が正しいか確認
   if (roomID.match(/^[AB]-\d{4}$/)) {
     // 部屋番号の形式が正しい場合、部屋DBに存在するか確認
-    let building = room.split("-")[0];
-    let floor = room.split("-")[1].substring(0, 2);
-    let roomNumber = room.split("-")[1].substring(2);
+    let building = roomID.split("-")[0];
+    let floor = roomID.split("-")[1].substring(0, 2);
+    let roomNumber = roomID.split("-")[1].substring(2);
 
     // 建物が存在するか確認
     if (roomDB[building]) {
@@ -44,10 +47,7 @@ app.get("/", (req, res) => {
 
 app.get("/api/roomData", async (req, res) => {
   try {
-    let roomDB = await fs.readFile(
-      `${__dirname}/db/roomData.json`,
-      "utf-8"
-    );
+    let roomDB = await fs.readFile(`${__dirname}/db/roomData.json`, "utf-8");
     roomDB = JSON.parse(roomDB);
 
     // 部屋番号から他の情報を取得する
@@ -56,12 +56,13 @@ app.get("/api/roomData", async (req, res) => {
       // 正しい部屋番号形式であり、その部屋が存在するか確認
       if (isAvailableRoomID(roomDB, roomID)) {
         let roomInfo = {};
+        roomInfo.correct = true;
         roomInfo.building = roomID.split("-")[0];
         roomInfo.floor = String(Number(roomID.split("-")[1].substring(0, 2)));
         roomInfo.room = roomID;
         return res.send(roomInfo);
       } else {
-        return res.send({});
+        return res.send({ correct: false });
       }
     }
 
@@ -132,7 +133,34 @@ app.get("/report", (req, res) => {
 });
 
 // 次回TODO。データ追加時にDiscord webhookで通知を送る
-app.post("/routeDataSubmit", async (req, res) => {});
+app.post("/api/routeDataSubmit", async (req, res) => {
+  // データの受け取り
+  console.log(req.body);
+  let { room, EV, stairs, orderOfPriority } = req.body;
+
+  // 教室番号チェック
+  let roomData = await fetch(
+    `http://localhost:${port}/api/roomData?room=${room}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  roomData = await roomData.json();
+  if (!roomData.correct) {
+    return res.status(400).send("Invalid room number.");
+  }
+
+  // DB書き込み
+  // TODO: 追加
+
+  // Discord webhook通知
+  // TODO: 追加
+
+  return res.status(200).send("Data submitted successfully.");
+});
 
 app.listen(port, function () {
   console.log(`[KGU EV Guide] Application Listening on Port ${port}`);
